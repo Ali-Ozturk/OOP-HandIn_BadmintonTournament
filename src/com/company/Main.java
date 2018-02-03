651d673891fada82931ea297640cd9a88918200b
Ali Ozturk <aliozturk@outlook.dk>
2018-02-03 15:33:45 +0100

    + Added more functionality in main file.    + Best player of match    + Best player stats + Renamed some methods and variables.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,26 +4,39 @@ public class Main {
     public static void main(String[] args) {
         Player dennis = new Player("Dennis", "Kaufmann", 26, 10, 5);
         Player jack = new Player("Jack", "Jones", 20, 5, 10);
-        Player cyka = new Player("Cyka", "Blyat", 19, 15, 0);
+        Player cyka = new Player("Cyka", "Blyat", 19, 15, 3);
         Player potato = new Player("Russian", "Potato", 30, 2, 10);
 
         Team america = new Team("America", dennis, jack);
         Team russia = new Team("Russia", cyka, potato);
 
         // Average age of team 1
-        double avgteam1 = america.AverageAge();
+        double AvgTeam1 = america.AverageAge();
         // Average age of team 2
-        double avgteam2 = russia.AverageAge();
+        double AvgTeam2 = russia.AverageAge();
+
+        // New line for easier use.
+        String newLine = System.getProperty("line.separator");
 
         System.out.println("Team statistics:");
-        System.out.println("Team 1 average age: " + avgteam1);
-        System.out.println("Team 2 average age: " + avgteam2);
-        System.out.println(" ");
+        System.out.println("Team 1 average age: " + AvgTeam1);
+        System.out.println("Team 2 average age: " + AvgTeam2 + newLine);
 
         Match1 match1 = new Match1(america, russia);
 
         System.out.println("Match 1 statistics:");
-        double avgageall = Match1.AverageAge();
+        double avgageall = match1.TeamAverageAge();
         System.out.println("The average age of match is: " + avgageall);
-        // TODO: Best player in match
+
+        Player BestPlayer = match1.BestPlayer();
+        System.out.println("The best player of Match 1 is: " + BestPlayer.firstname + " " + BestPlayer.lastname + newLine);
+
+        System.out.println("Best player statistics:");
+        System.out.println("Name: " + BestPlayer.firstname + " " + BestPlayer.lastname);
+        System.out.println("Age: " + BestPlayer.age);
+        System.out.println("W/L: " + BestPlayer.wonmatches + " / " + BestPlayer.lostmatches + " Ratio: " + BestPlayer.WinLoseRatio());
+        System.out.println("Current sponsor: " + BestPlayer.sponsor);
+
+        // TODO: Winner of match and update stats afterwards.
+    }
 }
--- src/com/company/Match1.java
+++ src/com/company/Match1.java
@@ -1,18 +1,19 @@
 package com.company;
 
 public class Match1 {
-    public Team Teamred;
-    public Team Teamblue;
+    private Team Teamred;
+    private Team Teamblue;
 
-    Match1(Team red, Team blue){
+    Match1(Team red, Team blue) {
         Teamred = red;
         Teamblue = blue;
     }
 
-    public double AverageAge() {
-        return((Teamred.player1.age + Teamred.player2.age + Teamblue.player1.age + Teamblue.player2.age) / 4.0);
+    public double TeamAverageAge() {
+        return ((Teamred.player1.age + Teamred.player2.age + Teamblue.player1.age + Teamblue.player2.age) / 4.0);
     }
 
     public Player BestPlayer() {
         return ((Teamblue.BestPlayer().WinLoseRatio() >= Teamred.BestPlayer().WinLoseRatio()) ? Teamblue.BestPlayer() : Teamred.BestPlayer());
     }
+}
